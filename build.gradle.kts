0673e7e8eb253f9081ace63579f8cfb774cb0b89
Guilherme Salustiano <guissalustiano@gmail.com>
2023-02-13 23:34:41 -0300

    config JOOQ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jooq.meta.jaxb.Logging
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -9,6 +11,7 @@ plugins {
     id("io.ktor.plugin") version "2.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
     id("org.flywaydb.flyway") version "9.14.1"
+    id("nu.studer.jooq") version "8.1"
 }
 
 group = "br.com.redosul"
@@ -29,7 +32,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("org.flywaydb:flyway-core:9.14.1")
-    implementation("org.postgresql:postgresql:$postgres_version")
+    jooqGenerator("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-resources:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
@@ -44,4 +47,43 @@ flyway {
     url = System.getenv("DB_URL")
     user = System.getenv("DB_USER")
     password = System.getenv("DB_PASSWORD")
+}
+
+jooq {configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(true)
+
+            jooqConfiguration.apply {
+                logging = Logging.INFO
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = System.getenv("DB_URL")
+                    user = System.getenv("DB_USER")
+                    password = System.getenv("DB_PASSWORD")
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        excludes = "flyway_schema_history"
+                        inputSchema = "public"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                        isRelations = true
+                        isPojos = true
+                        isPojosEqualsAndHashCode = true
+                        isDaos = true
+                    }
+                    target.apply {
+                        packageName = "br.com.redosul.generated"
+                    }
+                }
+            }
+        }
+    }
 }
